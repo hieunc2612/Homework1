fa74627ea4da5b0d5edffde0f7f70e5e3a4585c2
Hieu <nguyenconghieu@gmail.com>
2023-02-08 17:29:57 +0700

    Add project files.

---
+++ Homework1/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework1
+{
+    internal class Person
+    {
+        string name;
+        int age;
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public Person()
+        {
+
+        }
+        public Person(string name, int age)
+        {
+            Name = name;
+            if (age <= 0)
+            {
+                System.Console.WriteLine("Age cannot negative ");
+                System.Console.WriteLine("Therefore, the minimum age assigned is 0");
+                Age = 0;
+            }
+            else
+            {
+                Age = age;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "; Age: " + Age;
+        }
+    }
+}
---
+++ Homework1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter name: ");
+            string name = Console.ReadLine();
+            System.Console.WriteLine("Enter age: ");
+            int age = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("============");
+            Child child = new Child(name, age);
+            System.Console.WriteLine(child);
+            Console.WriteLine(child.ToString());
+        }
+    }
+}
